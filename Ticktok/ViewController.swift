b7f647520643de1a2eaf6aabc53919a9ded02b2b
brokolidev <bocalist@gmail.com>
2020-07-06 14:54:40 +0900

    Initial Commit

---
+++ Ticktok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ticktok
+//
+//  Created by Hyunseung Choi on 2020/07/06.
+//  Copyright © 2020 5s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
